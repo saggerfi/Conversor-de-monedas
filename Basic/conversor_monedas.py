287f138f723d70453db9b1e58eda3a27128cf411
sagger <yousagger@gmail.com>
2021-05-18 22:02:03 -0500

    Regresamos 3

--- Basic/conversor_monedas.py
+++ Basic/conversor_monedas.py
@@ -2,4 +2,4 @@ dolares = float(input("Dólares USD: "))
 pesos = 19.87
 resultado = round(dolares * pesos, 2)
 resultado = str(resultado)
-print("Tienes $" + resultado + "USD") 
+print("Tienes $" + resultado + "MXN") 
