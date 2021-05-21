917f9c6d9c19d7d4c728c014679f7a10f91f8923
sagger <yousagger@gmail.com>
2021-05-20 22:27:13 -0500

    20 MAYO 2021

--- Basic/conversor_monedas.py
+++ Basic/conversor_monedas.py
@@ -1,5 +1,35 @@
-dolares = float(input("Dólares USD: "))
-pesos = 19.87
-resultado = round(dolares * pesos, 2)
-resultado = str(resultado)
-print("Tienes $" + resultado + "MXN") 
+menu = """
+Bienvenido al conversor de monedas 💴
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion =  int(input(menu))
+
+if opcion == 1:
+    dolares = float(input("Dólares USD: "))
+    pesos = 3718.80
+    resultado = dolares * pesos
+    resultado = round(resultado, 2)
+    resultado = str(resultado)
+    print("Tienes $" + resultado + "COL") 
+elif opcion == 2:
+    dolares = float(input("Dólares USD: "))
+    pesos = 94.25
+    resultado = dolares * pesos
+    resultado = round(resultado, 2)
+    resultado = str(resultado)
+    print("Tienes $" + resultado + "ARG") 
+elif opcion == 3:
+    dolares = float(input("Dólares USD: "))
+    pesos = 19.87
+    resultado = dolares * pesos
+    resultado = round(resultado, 2)
+    resultado = str(resultado)
+    print("Tienes $" + resultado + "MXN") 
+else:
+    print("Muy gracioso, ingresa una opción valida")
+
