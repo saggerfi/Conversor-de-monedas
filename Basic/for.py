e477613e77737801817dc1d3a73af1a21170f070
sagger <yousagger@gmail.com>
2021-05-21 01:07:17 -0500

    21 MAYO 2021

--- Basic/for.py
+++ Basic/for.py
@@ -1,9 +1,10 @@
 def main():
-    contador = 1
-    print(contador)
-    for contador in range(1001):
-        print(contador)
-
+#     contador = 1
+#     print(contador)
+#     for contador in range(1001):
+#         print(contador)
+    for i in range(1, 10):
+        print(11 * i)
 
 if __name__ == '__main__':
     main()
---
+++ Basic/recorrer.py
@@ -0,0 +1,8 @@
+def main():
+    frase = input('Escribe una frase: ')
+    for letra in frase:
+        print(letra.upper())
+
+
+if __name__ == '__main__':
+    main()
